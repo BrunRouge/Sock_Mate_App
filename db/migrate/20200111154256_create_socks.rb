8ae2c93613e99c1387ec3f174bb1963bee687077
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-01-11 16:47:53 +0100

    integrating the sock migration

---
+++ db/migrate/20200111154256_create_socks.rb
@@ -0,0 +1,14 @@
+class CreateSocks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :socks do |t|
+      t.integer :size
+      t.integer :price
+      t.string :textile
+      t.string :color
+
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
