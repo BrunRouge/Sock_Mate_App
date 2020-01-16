8eb7bf8367bed5d65d92ac2aa16af48e2f389fc8
Anders Holstborg <jarmammail@gmail.com>
2020-01-16 20:55:15 +0100

    added create for sock + basic views for new, show + edit sock

--- app/controllers/socks_controller.rb
+++ app/controllers/socks_controller.rb
@@ -4,15 +4,25 @@ class SocksController < ApplicationController
   end
 
   def show
+    @sock = set_sock
   end
 
   def new
+    @sock = Sock.new
   end
 
   def create
+    @sock = Sock.new(sock_params)
+    @sock['user_id'] = current_user.id
+    if @sock.save
+      redirect_to sock_path(@sock)
+    else
+      render :new
+    end
   end
 
   def edit
+    @sock = set_sock
   end
 
   def update
--- app/models/sock.rb
+++ app/models/sock.rb
@@ -1,3 +1,7 @@
 class Sock < ApplicationRecord
   belongs_to :user
+  validates :textile, presence: true
+  validates :color, presence: true
+  validates :size, presence: true
+  validates :price, presence: true
 end
