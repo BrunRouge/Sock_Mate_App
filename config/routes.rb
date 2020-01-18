f238008dc60899660157fa678837998ed0de20fc
Anders Holstborg <jarmammail@gmail.com>
2020-01-18 11:57:11 +0100

    Added Bookings controller(unconfigured), views(unconfigured) and the pids-server fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :socks
-  resources :bookings
+  resources :bookings, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
