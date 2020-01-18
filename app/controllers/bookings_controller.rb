25bd5902dadb168be1a73f5f74961452511bd8ce
Anders Holstborg <jarmammail@gmail.com>
2020-01-18 13:38:20 +0100

    Re-added the pids-file and updated the name displayed in the browser heading + changed links to lead to root

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,36 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = find_booking
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(extended_booking_params)
+    if @booking.save!
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def extended_booking_params
+    booking_params.merge(user_id: current_user.id)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:sock_id)
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+end
