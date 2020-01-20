b6bc89944128ad85a86fc4973e43ae1df887d47c
Anders Holstborg <jarmammail@gmail.com>
2020-01-20 10:23:10 +0100

    Added min value for funds and updated migration to correctly store funds in Cents

--- db/migrate/20200118172901_add_funds_to_user.rb
+++ db/migrate/20200118172901_add_funds_to_user.rb
@@ -1,5 +1,5 @@
 class AddFundsToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :funds, :integer, :default => 100
+    add_column :users, :funds, :integer, :default => 10000
   end
 end
