1dad7147be15fe3cdaf7c54d5b24ca086de6baf9
Anders Holstborg <jarmammail@gmail.com>
2020-01-21 20:14:25 +0100

    Implemented transaction system /w error message in case of insufficient funds and notice of completed purchase

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,11 +10,13 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(extended_booking_params)
     @sock = Sock.find(params[:sock_id])
-    @booking.previousowner_id = @sock.user.id  # Mark previous owner for booking display
-    @booking.sellprice = @sock.price # Mark transfer price when booking is done (for reference)
-    @booking.save!
-    @sock.user_id = current_user.id # Transfer of ownership
-    @sock.save!
+    if payment
+      if transaction
+        flash[:notice] = "Transaction complete. Congratulations on your aquisition!"
+      else
+        flash[:notice] = "Oof, something went wrong here"
+      end
+    end
     redirect_to sock_path(@sock)
   end
 
@@ -30,5 +32,27 @@ class BookingsController < ApplicationController
 
   def find_booking
     @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def payment
+    if current_user.funds >= @sock.price
+      current_user.funds -= @sock.price
+      @sock.user.funds += @sock.price
+      current_user.save!
+      @sock.user.save!
+    else
+      flash[:notice] = "Insufficient funds"
+      return false
+    end
+  end
+
+  def transaction
+    @booking.previousowner_id = @sock.user.id  # Mark previous owner for booking display
+    @booking.sellprice = @sock.price # Mark transfer price when booking is done (for reference)
+    @booking.save!
+    @sock.user_id = current_user.id # Transfer of ownership
+    @sock.save!
   end
 end
